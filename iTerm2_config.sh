4bd7e020f8dd82959bea3263a9e8d28b280e4a4c
SAEMC <saemc27@gmail.com>
2022-04-19 00:32:55 +0900

    Create iTerm2_config.sh

---
+++ iTerm2_config.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Install Homebrew
+# Need to install XCode before
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Install ZSH
+brew install zsh
+
+# Install Oh-My-Zsh
+
+# Download D2Coding
+
+# Move iTerm2 config to ~/Library
