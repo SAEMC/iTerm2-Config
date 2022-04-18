e01d6a679f3dfc4ea1a3091ff763d5ad39319b5a
SAEMC <saemc27@gmail.com>
2022-04-19 00:50:37 +0900

    Update iTerm2_config.sh

--- iTerm2_config.sh
+++ iTerm2_config.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 
-# Install Homebrew
-# Need to install XCode before
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# check Homebrew
+brew_check="/bin/bash -c 'brew --version >/dev/null 2>&1'"
+brew_install="/bin/bash -c 'curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh'"
 
-# Install ZSH
-brew install zsh
+# Install Homebrew
+eval "$brew_check"
+
+if [[ "$?" -ne 0 ]]; then
+	eval "$brew_install"
+fi
+
+eval "$brew_check"
+
+if [[ "$?" -eq 0 ]]; then
+	# Install ZSH
+	brew install zsh
+fi
 
 # Install Oh-My-Zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # Modify ~/.zshrc
 # Change Theme to agnoster
+sed -i -e 's/ZSH_THEME=\"\"/ZSH_THEME=\"agnoster\"/g' test.txt
 
 # plugins=(git git-prompt)
 
